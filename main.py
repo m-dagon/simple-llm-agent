bd3154e3cab2b4724b96a7f30c0da8d06c5db28a
mehrunes <mehrunesdagon737@gmail.com>
2025-07-23 12:43:20 -0500

    Added verbose option and message history.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import os
 from dotenv import load_dotenv
 
@@ -5,17 +6,33 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 from google import genai
+from google.genai import types
 
 client = genai.Client(api_key=api_key)
 
 
 def main():
-    #print("Hello from simple-llm-agent!")
-    model = "gemini-2.0-flash-001"
-    content = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    response = client.models.generate_content(model=model, contents=content)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if not len(sys.argv) >= 2:
+        exit(1)
+
+    user_prompt = str(sys.argv[1])
+    verbose_flag = len(sys.argv) >= 3 and sys.argv[2] == "--verbose"
+    gemini_model = "gemini-2.0-flash-001"
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    response = client.models.generate_content(
+        model=gemini_model,
+        contents=messages,
+    )
+    
+    if verbose_flag:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
     print(response.text)
 
 
